8aa083c78076e2765e375f8d100ce246d1293c1d
TeamSoftTelegram <teamsofttg@gmail.com>
2016-05-12 18:24:49 +0430

    Update weather.lua

--- plugins/weather.lua
+++ plugins/weather.lua
@@ -27,7 +27,7 @@ local function get_weather(location)
     conditions = conditions .. 'مه 🌫'
   end
 
-  return temp .. '\n\n' .. conditions..'\n\n@WaderTGTeam'
+  return temp .. '\n\n' .. conditions..'\n\n@SofTG'
 end
 local function run(msg, matches) 
     city = matches[1]
