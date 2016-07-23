f0d6ccb90804bbdbaa49a8cdd2d2c085ea5c541a
PARADOXTEAM <sajad.yas1377@gmail.com>
2016-07-23 15:50:14 +0430

    Update server.lua

--- plugins/server.lua
+++ plugins/server.lua
@@ -3,16 +3,16 @@ do
 function run(msg, matches)
   if matches[1] == "reboot" and is_sudo(msg) then
         return 'This plugin is disable'  -- Dont enable it,if you use Free online servers!
-  elseif matches[1] == "serverinfo" and is_sudo(msg) then
+  elseif matches[1] == "paradox" and is_sudo(msg) then
                       -- change this location if you use another source/location !
-     local f = io.popen("sh /root/WaderTG/data/server/serverinfo.sh") 
+     local f = io.popen("sh /root/ParadoxBot/data/server/serverinfo.sh") 
      return ( f:read("*a") ) --read all of the results and return it !
   end
 end
 return {
   patterns = {
     "^[#/!](reboot)",
-    "^[#/!](serverinfo)",
+    "^[#/!]([Pp]aradox)",
   },
   run = run
 }
